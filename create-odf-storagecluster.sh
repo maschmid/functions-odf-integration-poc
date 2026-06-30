459eefb1c86d4471088217c0e414f5b49463a090
Marek Schmidt <maschmid@redhat.com>
2026-06-30 16:49:58 +0200

    storagecluster creation on openstack

--- create-odf-storagecluster.sh
+++ create-odf-storagecluster.sh
@@ -2,6 +2,22 @@
 
 set -Eeuxo pipefail
 
+orig=$(oc get machineset -n openshift-machine-api -o name | grep 'worker-' | head -n1 | sed 's|machineset.machine.openshift.io/||')
+kind=$(oc get machineset -n openshift-machine-api "$orig" -o json | jq -r .spec.template.spec.providerSpec.value.kind)
+
+if [ "$kind" = "OpenstackProviderSpec" ]; then
+  storage_class=standard-csi
+  storage_size=128Gi
+  deviceset_name=ocs-deviceset-standard-csi
+elif [ "$kind" = "AWSMachineProviderConfig" ]; then
+  storage_class=gp3-csi
+  storage_size=128Gi
+  deviceset_name=ocs-deviceset-gp3-csi
+else
+  echo "Unknown provider kind: $kind"
+  exit 1
+fi
+
 function create_odf_storagecluster() {
   oc create -f - <<EOF
 apiVersion: ocs.openshift.io/v1
@@ -45,12 +61,12 @@ spec:
         - ReadWriteOnce
         resources:
           requests:
-            storage: 128Gi
-        storageClassName: gp3-csi
+            storage: ${storage_size}
+        storageClassName: ${storage_class}
         volumeMode: Block
       status: {}
     deviceClass: ssd
-    name: ocs-deviceset-gp3-csi
+    name: ${deviceset_name}
     placement: {}
     portable: true
     preparePlacement: {}
